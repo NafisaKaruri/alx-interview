b321c90f79195e08a06bacbff5dca7049b3fbae2
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-23 23:28:38 +0000

    modified

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-log parsing
+Log Parsing Script
 """
 
 import sys
@@ -9,42 +9,45 @@ import re
 
 def output(log: dict) -> None:
     """
-    helper function to display stats
+    Helper function to display stats
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
+        if log["code_frequency"][code] > 0:
             print("{}: {}".format(code, log["code_frequency"][code]))
 
 
 if __name__ == "__main__":
     regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+        r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\] '
+        r'"GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'
+    )
 
     line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
+    log = {
+        "file_size": 0,
+        "code_frequency": {str(code): 0 for code in [
             200, 301, 400, 401, 403, 404, 405, 500]}
+    }
 
     try:
         for line in sys.stdin:
             line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
+            match = regex.match(line)
+            if match:
                 line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+                code = match.group(2)
+                file_size = int(match.group(3))
 
-                # File size
                 log["file_size"] += file_size
 
-                # status code
-                if (code.isdecimal()):
+                if code in log["code_frequency"]:
                     log["code_frequency"][code] += 1
 
-                if (line_count % 10 == 0):
+                if line_count % 10 == 0:
                     output(log)
+    except KeyboardInterrupt:
+        output(log)
     finally:
         output(log)
