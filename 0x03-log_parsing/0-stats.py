ad6cb48af16d1b057c08756c655d22af5d1917ea
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-25 23:25:32 +0000

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -36,16 +36,14 @@ if __name__ == "__main__":
             line = line.strip()
             match = regex.match(line)
             if match:
-                line_count += 1
                 code = match.group(2)
                 file_size_str = match.group(3)
 
                 try:
                     file_size = int(file_size_str)
+                    log["file_size"] += file_size
                 except ValueError:
                     continue  # Skip lines with invalid file size
-
-                log["file_size"] += file_size
 
                 if code in log["code_frequency"]:
                     log["code_frequency"][code] += 1
