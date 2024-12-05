59742c7d41b430aa6ffae9cdf1c1645d5f832cdd
NafisaKaruri <nafeesahassan016@gmail.com>
2024-12-05 00:57:05 +0000

    intend 2spaces

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,16 @@
 
 const request = require('request');
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res, body) => {
-	if (err) throw err;
-	const actors = JSON.parse(body).characters;
-	exactOrder(actors, 0);
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
 
 const exactOrder = (actors, x) => {
-	if (x === actors.length) return;
-	request(actors[x], (err, res, body) => {
-		if (err) throw err;
-		console.log(JSON.parse(body).name);
-		exactOrder(actors, x + 1);
-	});
+  if (x === actors.length) return;
+  request(actors[x], (err, res, body) => {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
 };
