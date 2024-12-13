0cc05a00981541992a70be715e4acd9fd0c8b3fe
NafisaKaruri <nafeesahassan016@gmail.com>
2024-12-12 16:44:45 -0800

    wtf

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,67 +1,42 @@
 #!/usr/bin/python3
-"""Module defining isWinner function for determining the winner."""
+"""0. Prime Game - Maria and Ben are playing a game"""
 
 
 def isWinner(x, nums):
-    """Determine who wins most rounds in the prime game.
-
-    Args:
-        x (int): Number of rounds.
-        nums (list): List of integers for each round's n.
-
-    Returns:
-        str: Winner's name ("Maria" or "Ben"), or None if tie.
+    """x - rounds
+    nums - numbers list
     """
-    if x <= 0 or nums is None or x != len(nums):
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
         return None
 
-    mariaWinsCount = 0  # Count of Maria's wins
-    benWinsCount = 0  # Count of Ben's wins
+    ben = 0
+    maria = 0
 
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
 
-        if not primesSet:  # Ben wins if no primes
-            benWinsCount += 1
-            continue
-
-        isMariaTurns = True  # Flag for Maria's turn
-
-        while(True):
-            if not primesSet:  # If no primes left, the current player loses
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns  # Switch turns
-
-    if mariaWinsCount > benWinsCount:
-        return "Maria"
-
-    if mariaWinsCount < benWinsCount:
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
         return "Ben"
-
-    return None  # Return None if there is a tie
-
-
-def is_prime(n):
-    """Return True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+    if maria > ben:
+        return "Maria"
+    return None
 
 
-def primes_in_range(start, end):
-    """Return list of primes in the range [start, end]."""
-    return [n for n in range(start, end + 1) if is_prime(n)]
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
